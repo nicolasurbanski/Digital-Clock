409e31ebd637217fc8b87800e0e485dc15b388c8
urbanskinico <urbanskinico@gmail.com>
2023-04-02 23:42:14 -0300

    inicializacion

---
+++ main.js
@@ -0,0 +1,21 @@
+const clock = document.getElementById(`Clock`)
+
+
+const currentTime=()=>{
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    
+    hh = hh < 10 ? `0${hh}`: hh;
+    mm = mm < 10 ? `0${mm}`: mm;
+    ss = ss < 10 ? `0${ss}`: ss;
+
+    let time = `${hh}:${mm}:${ss}`
+
+    clock.innerHTML = time;
+
+}
+
+clock && currentTime();
+setInterval(currentTime, 1000)
